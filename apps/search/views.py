ed267ce5d78d297bbd8a1e531533db1cd5e792bd
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-07-30 23:41:37 -0400

    almost working

--- apps/search/views.py
+++ apps/search/views.py
@@ -5,7 +5,9 @@ def index(request):
     return render(request, "index.html")
 
 def search(request):
-    return redirect("http://google.com")
+    if request.method == 'POST':
+        search = request.form["real_search"]
+    return redirect(f"https://www.google.com/search?safe=off&source=hp&ei=WA1BXbWJB6eB5wKlmovoDQ&q={search}")
 
 def real_search(request):
     if request.method == 'POST':
