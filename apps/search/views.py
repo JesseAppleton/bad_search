57373836ce5c284b15157b758a6791e7b7503b6f
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-07-31 17:03:54 -0400

    finished build

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, redirect
-from django.utils.crypto import get_random_string
-from .models import *
+import random
+import urllib.request
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-
-def search(request):
-    if request.method == 'POST':
-        search = request.POST["search_input"][0]
-        word = get_random_string(length=4)
-    return redirect(f"https://www.google.com/search?safe=off&source=hp&ei=WA1BXbWJB6eB5wKlmovoDQ&q={search}{word}")
-
+    
 def real_search(request):
-    if request.method == 'POST':
-        search = request.POST["search_input"]
-        print(search)
+    search = "How to use google like a normal person"
     return redirect(f"https://lmgtfy.com/?q={search}")
 
-''' TO DO:
-        make words related to the first letter and reference them at random
-        make real_search work correctly
-            second modal based on the bottom of the screen?
-                footer for bootstrap
-        finish styling
+def search(request):
+    search_list = []
+    if request.method == 'POST':
+        search = request.POST["search_input"][0].lower()
+        word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+        response = urllib.request.urlopen(word_url)
+        long_txt = response.read().decode()
+        for i in long_txt.splitlines():
+            if i[0] == search:
+                search_list.append(i)
+        words = random.choice(search_list)
+    return redirect(f"https://www.google.com/search?safe=off&source=hp&ei=WA1BXbWJB6eB5wKlmovoDQ&q={words}")
