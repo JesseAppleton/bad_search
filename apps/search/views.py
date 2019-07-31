317246c334980d0bbc2617bf55ba9ea6ef665cf3
JESSEPC\Jesseyo <appleton.jesse@gmail.com>
2019-07-31 00:28:23 -0400

    first mostly working build

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -6,10 +8,19 @@ def index(request):
 
 def search(request):
     if request.method == 'POST':
-        search = request.form["real_search"]
-    return redirect(f"https://www.google.com/search?safe=off&source=hp&ei=WA1BXbWJB6eB5wKlmovoDQ&q={search}")
+        search = request.POST["search_input"][0]
+        word = get_random_string(length=4)
+    return redirect(f"https://www.google.com/search?safe=off&source=hp&ei=WA1BXbWJB6eB5wKlmovoDQ&q={search}{word}")
 
 def real_search(request):
     if request.method == 'POST':
-        search = request.form["real_search"]
+        search = request.POST["search_input"]
+        print(search)
     return redirect(f"https://lmgtfy.com/?q={search}")
+
+''' TO DO:
+        make words related to the first letter and reference them at random
+        make real_search work correctly
+            second modal based on the bottom of the screen?
+                footer for bootstrap
+        finish styling
--- bad_search/urls.py
+++ bad_search/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
+
 from django.urls import path
 from apps.search import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index),
     path('search', views.search),
     path('real_search', views.real_search),
